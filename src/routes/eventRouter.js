2bb9373a909109d1e55f7502cff32f1a4735c5c6
Alvin Wang <alvinwang922@gmail.com>
2020-05-03 15:19:17 -0700

    Tagging

--- src/routes/eventRouter.js
+++ src/routes/eventRouter.js
@@ -6,48 +6,46 @@ const { getDatabase } = require('../database/mongo');
 
 // GET JSON of all events
 routes.get('/all', async (req, res, next) => {
-
-    Event.find(function (err, events) {
-        if (err) {
-          console.log('error ', err.res);
-        }
-        else {
-          res.status(200).json(events);
-        }
-      })
+  Event.find(function(err, events) {
+    if (err) {
+      console.log('error ', err.res);
+    } else {
+      res.status(200).json(events);
+    }
+  });
 });
-
 
 // POST a new event to the database
 routes.post('/event', (request, res) => {
-    if (!request.body || request.body == {}){
-      return res.send("no request body")
-    }
-    var event = new Event({
-        title: request.body.title,
-        location: request.body.location,
-        startTime: request.body.startTime,
-        endTime: request.body.endTime,
-        checkIn: request.body.checkIn,
-        url: request.body.url
-    }).save((err, response) => {
-        if (err) res.status(400).send(err)
-        res.status(200).send(response)
-    })
-    
+  if (!request.body || request.body == {}) {
+    return res.send('no request body');
+  }
+  var event = new Event({
+    title: request.body.title,
+    location: request.body.location,
+    startTime: request.body.startTime,
+    endTime: request.body.endTime,
+    checkIn: request.body.checkIn,
+    tags: request.body.tags,
+    url: request.body.url
+  }).save((err, response) => {
+    if (err) res.status(400).send(err);
+    res.status(200).send(response);
+  });
 });
 
 // GET specific events from the database via a "query" (title search, description, time, etc.)
-routes.get("/event/:query", (request, res) => {
-    var event = Event.find({ $text: { $search: request.params.query}}, function(err, events){
-      if (err) {
-        console.log('error ', err);
-      }
-      else {
-        res.status(200).json(events);
-      }
-    });
-})
-
+routes.get('/event/:query', (request, res) => {
+  var event = Event.find({ $text: { $search: request.params.query } }, function(
+    err,
+    events
+  ) {
+    if (err) {
+      console.log('error ', err);
+    } else {
+      res.status(200).json(events);
+    }
+  });
+});
 
 module.exports = routes;
