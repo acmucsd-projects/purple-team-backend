a57e308f5c06d5a09af9730062c5563b999eb4af
Alvin Wang <alvinwang922@gmail.com>
2020-05-06 13:58:09 -0700

    Tagging (#4)
    Added tagging and implemented deleting events

--- src/database/models/eventsModel.js
+++ src/database/models/eventsModel.js
@@ -2,34 +2,37 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 //Event model consisting of the event title, description, start and end time, checkIn code, and splash URL
-var eventSchema = new Schema(
-    {
-    title: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    startTime: {
-        type: Date,
-        required: true
-    },
-    endTime: {
-        type: Date,
-        required: true
-    },
-    checkIn: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: false
-    }
-})
+var eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  },
+  checkIn: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: Array,
+    required: true
+  },
+  url: {
+    type: String,
+    required: false
+  }
+});
 
-const Event = mongoose.model("Event", eventSchema);
+const Event = mongoose.model('Event', eventSchema);
 
-module.exports = {Event};
+module.exports = { Event };
--- src/routes/eventRouter.js
+++ src/routes/eventRouter.js
@@ -6,48 +6,61 @@ const { getDatabase } = require('../database/mongo');
 
 // GET JSON of all events
 routes.get('/all', async (req, res, next) => {
-
-    Event.find(function (err, events) {
-        if (err) {
-          console.log('error ', err.res);
-        }
-        else {
-          res.status(200).json(events);
-        }
-      })
+  Event.find(function(err, events) {
+    if (err) {
+      console.log('error ', err.res);
+    } else {
+      res.status(200).json(events);
+    }
+  });
 });
-
 
 // POST a new event to the database
 routes.post('/event', (request, res) => {
-    if (!request.body || request.body == {}){
-      return res.send("no request body")
-    }
-    var event = new Event({
-        title: request.body.title,
-        location: request.body.location,
-        startTime: request.body.startTime,
-        endTime: request.body.endTime,
-        checkIn: request.body.checkIn,
-        url: request.body.url
-    }).save((err, response) => {
-        if (err) res.status(400).send(err)
-        res.status(200).send(response)
-    })
-    
+  if (!request.body || request.body == {}) {
+    return res.send('no request body');
+  }
+  var event = new Event({
+    title: request.body.title,
+    location: request.body.location,
+    startTime: request.body.startTime,
+    endTime: request.body.endTime,
+    checkIn: request.body.checkIn,
+    tags: request.body.tags,
+    url: request.body.url
+  }).save((err, response) => {
+    if (err) res.status(400).send(err);
+    res.status(200).send(response);
+  });
+});
+
+// @route    DELETE event
+// @desc     Delete an event
+// @access   Private
+routes.delete('/event', async (req, res) => {
+  try {
+    // Remove event
+    await Event.findOneAndRemove({ event: req.body.id });
+
+    res.json({ msg: 'Event deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // GET specific events from the database via a "query" (title search, description, time, etc.)
-routes.get("/event/:query", (request, res) => {
-    var event = Event.find({ $text: { $search: request.params.query}}, function(err, events){
-      if (err) {
-        console.log('error ', err);
-      }
-      else {
-        res.status(200).json(events);
-      }
-    });
-})
-
+routes.get('/event/:query', (request, res) => {
+  var event = Event.find({ $text: { $search: request.params.query } }, function(
+    err,
+    events
+  ) {
+    if (err) {
+      console.log('error ', err);
+    } else {
+      res.status(200).json(events);
+    }
+  });
+});
 
 module.exports = routes;
